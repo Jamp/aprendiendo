2f30c345395e814153db58c3aab72f8929c7608d
Jaro Marval <jampgold@gmail.com>
2013-05-21 16:51:01 -0430

    Conexión a base de datos con mysql_connect, no lo hagan es totalmente educativo

--- index.php
+++ index.php
@@ -1,6 +1,20 @@
 <?php
-echo "Hola mundo!!!";
-for ($i=0; $i < 500; $i++) {
-	echo "Hola experimental\n";
+
+$conexion = mysql_connect('127.0.0.1', 'root', ''); // localhost = 127.0.0.1
+
+if ($conexion) {
+	echo "Conectado<br>";
+	mysql_select_db('prueba');
+	$result = mysql_query("SELECT * FROM `prueba`");
+
+	while ($fila = mysql_fetch_object($result)) {
+		echo "ID: " , $fila->id , " Nombre: " ,
+		$fila->nombre , " Informaci&oacute;n: " ,
+		$fila->informacion , "<br>";
+	}
+
+	mysql_close($conexion);
+	echo "Cerrado<br>";
 }
+
 ?>
